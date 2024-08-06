14360d8fce395a42c6b5ff1748d9a866efdb4830
Guy Wilson <Guywilsonjr@gmail.com>
2024-08-05 18:38:33 -0700

    Shutdown after done

--- tests/docker/entry.sh
+++ tests/docker/entry.sh
@@ -1,7 +1,7 @@
 echo "Hey, I'm running in a container!" >> /test/echo_command.log
 .venv/bin/tox -e py312
 systemctl list-unit-files local_cloud_agent.service
-reboot
+shutdown 0
 #systemctl restart test_startup.service
 # , "-p", "all", "--parallel-live"
 #--tmpfs /tmp --tmpfs /run --tmpfs /run/lock -v /sys/fs/cgroup:/sys/fs/cgroup:ro
